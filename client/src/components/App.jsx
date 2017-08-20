1bacf3cf825f656d507b0383b8a4bfe73719dc7a
universalmind <universalmind.candy@gmail.com>
2017-08-20 12:51:09 -0500

    GmailAPI not implemented, everything else working correctly

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -7,7 +7,7 @@ export default () => (
     <Navbar />
     <EmailForm />
     <div>
-    If you wish to continue with the interview process, It is as easy as sending an email!
+    If you like what you see, I would love to hear what you think. 
     </div>
 
     <button type="button" className="btn btn-primary" data-toggle="modal" data-target="#sendEmail">Compose email</button>
